95d0657d602c772c3d55de430e45c0f60a8ea5f4
Arkana Bilal Imani <arkana2003@gmail.com>
2024-06-08 20:12:48 +0700

    08/06/2024 Preliminary Profile and editProfile Layouting

---
+++ app/src/main/java/com/example/finalproject_cthru/view/profile/ProfileEditActivity.kt
@@ -0,0 +1,39 @@
+package com.example.finalproject_cthru.view.profile
+
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.finalproject_cthru.R
+import com.example.finalproject_cthru.databinding.ActivityProfileEditBinding
+import com.example.finalproject_cthru.databinding.ActivityRegisterBinding
+
+class ProfileEditActivity : AppCompatActivity() {
+
+    private  lateinit var  binding: ActivityProfileEditBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+}
