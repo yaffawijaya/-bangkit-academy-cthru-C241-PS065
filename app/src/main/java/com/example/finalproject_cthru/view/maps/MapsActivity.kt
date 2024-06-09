c3b305eb57e5f13c173edf709bad9ba84b5fde3e
TamaFn <sandya.fransisna@gmail.com>
2024-06-09 14:35:07 +0700

    01/06/2024 - Adding GMaps Feature

---
+++ app/src/main/java/com/example/finalproject_cthru/view/maps/MapsActivity.kt
@@ -0,0 +1,161 @@
+package com.example.finalproject_cthru.view.maps
+
+import android.content.pm.PackageManager
+import android.content.res.Resources
+import android.location.Location
+import android.os.Bundle
+import android.util.Log
+import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.finalproject_cthru.R
+import com.example.finalproject_cthru.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+    private var isTrafficEnable: Boolean = false
+    private lateinit var currentLocation: Location
+    private var radius = 1500
+    private val locationViewModel: MapsViewModel by viewModels<MapsViewModel>()
+    private val boundsBuilder = LatLngBounds.Builder()
+
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                getMyLocation()
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setTitle(R.string.maps)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        mMap.uiSettings.apply {
+            isZoomControlsEnabled = true
+            isCompassEnabled = true
+            isIndoorLevelPickerEnabled = false
+            isMapToolbarEnabled = false
+            isMyLocationButtonEnabled = false
+        }
+
+
+        getMyLocation()
+        setMapStyle()
+        setTraffic()
+//        getMapStory()
+
+
+    }
+
+    private fun getMyLocation() {
+        if (ContextCompat.checkSelfPermission(
+                this.applicationContext,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            binding.currentLocation.setOnClickListener {
+                mMap.isMyLocationEnabled = true
+                fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                    if (location!= null) {
+                        val currentLatLng = LatLng(location.latitude, location.longitude)
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
+                        mMap.addMarker(MarkerOptions().position(currentLatLng).title("Your Location"))
+                    } else {
+                        Toast.makeText(this, "Location not found", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        } else {
+            requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    private fun setMapStyle() {
+        try {
+            val success =
+                mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style))
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.")
+            }
+        } catch (exception: Resources.NotFoundException) {
+            Log.e(TAG, "Can't find style. Error: ", exception)
+        }
+
+        binding.btnMapType.setOnClickListener {
+            val popupMenu = PopupMenu(this@MapsActivity, it)
+
+            popupMenu.apply {
+                menuInflater.inflate(R.menu.map_type_menu, popupMenu.menu)
+                setOnMenuItemClickListener { item ->
+                    when (item.itemId) {
+                        R.id.btnNormal -> mMap?.mapType = GoogleMap.MAP_TYPE_NORMAL
+                        R.id.btnSatellite -> mMap?.mapType = GoogleMap.MAP_TYPE_SATELLITE
+                        R.id.btnTerrain -> mMap?.mapType = GoogleMap.MAP_TYPE_TERRAIN
+                    }
+                    true
+                }
+
+                show()
+            }
+        }
+    }
+
+    private fun setTraffic(){
+        binding.enableTraffic.setOnClickListener {
+
+            if (isTrafficEnable) {
+                mMap?.apply {
+                    isTrafficEnabled = false
+                    isTrafficEnable = false
+                }
+            } else {
+                mMap?.apply {
+                    isTrafficEnabled = true
+                    isTrafficEnable = true
+                }
+            }
+        }
+    }
+
+
+
+    companion object {
+        private const val TAG = "MapsActivity"
+    }
+}
